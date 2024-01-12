fc8504d0f28f58a77a7c7a95d9b34da7f5499df2
创圣大天使Evol <296662402@qq.com>
2024-01-12 09:08:50 +0800

    display the post[id].tsx

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const Post: React.FC<PostProps> = (props) => {
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   // const { data: session, status } = useSession();
   // if (status === "loading") {
   //   return <div>Authenticating ...</div>;
@@ -14,8 +14,8 @@ const Post: React.FC<PostProps> = (props) => {
   const userHasValidSession = true;
   const postBelongsToUser =
     true; /* session?.user?.email === props.author?.email */
-  let title = props.title;
-  if (!props.published) {
+  let title = post.title;
+  if (!post.published) {
     title = `${title} (Draft)`;
   }
 
@@ -24,13 +24,14 @@ const Post: React.FC<PostProps> = (props) => {
 
   return (
     <>
-      <div>
-        <h2 className="mb-4 text-2xl font-bold">{title}</h2>
-        <p className="text-gray-600">
-          By {props?.author?.name || "Unknown author"}
-        </p>
-        <ReactMarkdown className="mt-4" children={props.content} />
-        {!props.published && userHasValidSession && postBelongsToUser && (
+      <div className="ml-8">
+        <div className="flex flex-col space-y-4">
+          <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+          <p className="text-gray-600">
+            By {post?.author?.name || "Unknown author"}
+          </p>
+          <ReactMarkdown className="mt-4" children={post.content} />
+          {/* {!props.published && userHasValidSession && postBelongsToUser && (
           <button
             onClick={() => publishPost(props.id)}
             className="rounded bg-gray-300 px-4 py-2 text-gray-700"
@@ -45,7 +46,8 @@ const Post: React.FC<PostProps> = (props) => {
           >
             Delete
           </button>
-        )}
+        )} */}
+        </div>
       </div>
     </>
   );
@@ -66,7 +68,7 @@ const post1: PostProps = {
 const Page = () => {
   return (
     <div>
-      <Post props={post1} />
+      <Post post={post1} />
     </div>
   );
 };
