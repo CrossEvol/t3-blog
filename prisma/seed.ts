cb1c3c177471360643cf82f86d0a6a54fcb25d88
创圣大天使Evol <296662402@qq.com>
2024-01-12 11:31:44 +0800

    Init User & Posts data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,40 @@
-console.log('Prisma Seed')
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const user = await prisma.user.create({
+    data: {
+      name: faker.internet.userName(),
+      password: faker.string.alphanumeric(10),
+      email: faker.internet.email(),
+      image: faker.internet.avatar(),
+    },
+  });
+
+  for (let i = 0; i < 10; i++) {
+    await prisma.post.create({
+      data: {
+        name: faker.string.alpha({ length: { min: 10, max: 20 } }),
+        title: faker.string.alpha({ length: { min: 10, max: 20 } }),
+        content: faker.string.alpha({ length: { min: 20, max: 50 } }),
+        published: false,
+        author: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+  }
+};
+
+main()
+  .then((res) => {
+    console.log("Init Data finished.");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
