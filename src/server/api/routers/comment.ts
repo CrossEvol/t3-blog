e5bb5c7a1d2a0f12e928754b1d630d0f53ea499e
创圣大天使Evol <296662402@qq.com>
2024-01-13 21:23:59 +0800

    comment.Create

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -6,4 +6,24 @@ import {
   publicProcedure,
 } from "@/server/api/trpc";
 
-export const commentRouter = createTRPCRouter({});
+export const commentRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        text: z.string().min(0),
+        postId: z.number().min(0),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx.session;
+
+      return ctx.db.comment.create({
+        data: {
+          created_at: Date.now(),
+          userId: user.id,
+          postId: input.postId,
+          text: input.text,
+        },
+      });
+    }),
+});
