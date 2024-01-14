1ae8887fc8babff83fff190fb514a35a4b64400d
创圣大天使Evol <296662402@qq.com>
2024-01-14 15:27:40 +0800

    Extract ShowPost

---
+++ src/app/post/[id]/ShowPost.tsx
@@ -0,0 +1,29 @@
+import { PostItem } from "@/app/page";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  post: PostItem;
+}
+
+const ShowPost = ({ post }: Props) => {
+  let title = post.title;
+  if (!post.published) {
+    title = `${title} (Draft)`;
+  }
+
+  return (
+    <>
+      <div className="ml-8">
+        <div className="flex flex-col space-y-4">
+          <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+          <p className="text-gray-600">
+            By {post?.author?.name || "Unknown author"}
+          </p>
+          <ReactMarkdown className="mt-4" children={post.content} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ShowPost;
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,36 +1,10 @@
-import { PostItem } from "@/app/page";
 import CommentFormWrapper from "@/app/post/_comment/CommentFormWrapper";
 import CommentList from "@/app/post/_comment/CommentList";
 import Container from "@/app/post/_comment/Container";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
-import ReactMarkdown from "react-markdown";
 import PostActions from "./PostActions";
-
-interface Props {
-  post: PostItem;
-}
-
-const ShowPost = ({ post }: Props) => {
-  let title = post.title;
-  if (!post.published) {
-    title = `${title} (Draft)`;
-  }
-
-  return (
-    <>
-      <div className="ml-8">
-        <div className="flex flex-col space-y-4">
-          <h2 className="mb-4 text-2xl font-bold">{title}</h2>
-          <p className="text-gray-600">
-            By {post?.author?.name || "Unknown author"}
-          </p>
-          <ReactMarkdown className="mt-4" children={post.content} />
-        </div>
-      </div>
-    </>
-  );
-};
+import ShowPost from "./ShowPost";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const post = await api.post.getOne.query({ id: Number(params.id) });
