0816dbcb08aca5391498b887c8b286288bcb3158
创圣大天使Evol <296662402@qq.com>
2024-08-28 01:11:00 +0800

    solve hydration error for <div/> in <a/>. use parent relative and child relative, then <Post/> can be full rendered in server. try to set overflow for <Post/>

--- src/app/_components/Post.tsx
+++ src/app/_components/Post.tsx
@@ -1,23 +1,15 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import type { PostItem } from "../page";
 
 const Post: React.FC<{ post: PostItem }> = ({ post }) => {
-  const router = useRouter();
   const authorName = post.author ? post.author.name : "Unknown author";
   return (
-    <div
-      className="cursor-pointer"
-      onClick={() => router.push(`/post/${post.id}`)}
-    >
-      <div className="m-2 flex flex-col space-y-6 bg-inherit px-8 py-12 text-inherit">
-        <h2 className="text-2xl font-semibold">{post.title}</h2>
-        <small>By {authorName}</small>
-        <ReactMarkdown>{post.content}</ReactMarkdown>
-      </div>
+    <div className="relative m-2 flex max-h-96 flex-col space-y-6 overflow-y-scroll bg-inherit px-8 py-12 text-inherit">
+      <h2 className="text-2xl font-semibold">{post.title}</h2>
+      <small>By {authorName}</small>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <a href={`/post/${post.id}`} className="absolute inset-0 z-10" />
     </div>
   );
 };
