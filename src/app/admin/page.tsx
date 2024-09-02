5b0346a942260c7bf45b36f0aa215f60ded3aa59
创圣大天使Evol <296662402@qq.com>
2024-09-02 11:54:08 +0800

    <AdminPortal/> for /admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { Terminal } from 'lucide-react'
 
-const Page = () => {
-  return <div>Admin</div>
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import Link from 'next/link'
+
+export function AdminPortal() {
+  return (
+    <div className="container space-y-4 min-h-96">
+      <div className="hover:bg-slate-600">
+        <Link href={`/admin/topic`}>
+          <Alert>
+            <Terminal className="h-4 w-4" />
+            <AlertTitle>Topic</AlertTitle>
+            <AlertDescription>
+              You can edit/delete/sort/filter topics here.
+            </AlertDescription>
+          </Alert>
+        </Link>
+      </div>
+      <div className="hover:bg-slate-600">
+        <Link href={`/admin/tag`}>
+          <Alert>
+            <Terminal className="h-4 w-4" />
+            <AlertTitle>Tag</AlertTitle>
+            <AlertDescription>
+              You can edit/delete/sort/filter tags here.
+            </AlertDescription>
+          </Alert>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default Page
+export default AdminPortal
